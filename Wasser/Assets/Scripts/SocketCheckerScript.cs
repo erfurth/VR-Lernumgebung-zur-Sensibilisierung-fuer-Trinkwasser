b80944190344406332cfece751ea8e90f1a53ee0
Isa <Isabell.Noack@stud.eah-jena.de>
2024-07-12 20:40:21 +0200

    Weitere Elemente Wasserkreislauf

--- Wasser/Assets/Scripts/SocketCheckerScript.cs
+++ Wasser/Assets/Scripts/SocketCheckerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class SocketChecker : MonoBehaviour
 {
     public GameObject correctObject; // Gewünschtes Objekt
+    public GameObject[] ToggleObjectState; 
     private XRSocketInteractor socketInteractor;
     private void Awake() //aufgerufen, bevor das Spielt gestartet wird
     {
@@ -14,17 +15,30 @@ public class SocketChecker : MonoBehaviour
         {
             Debug.LogError("Kein XRSocketInteractor auf diesem GameObject");
         }
+
+        for (int i = 0; i < ToggleObjectState.Length; i++)
+        {
+            ToggleObjectState[i].SetActive(false);
+        }
     }
 
     public void OnObjectEntered(SelectEnterEventArgs args)
     {   
         XRGrabInteractable grabInteractable = args.interactableObject as XRGrabInteractable;
         if (grabInteractable != null && grabInteractable.gameObject == correctObject)
-        {
+        {         
             //Debug.Log("Das korrekte Objekt wurde in den Socket gelegt");
             AnimationScript animationScript = grabInteractable.gameObject.GetComponent<AnimationScript>();
-            //Debug.Log("Checker: spielt ab!");
-            animationScript.Abspielen();
+
+            if (animationScript != null){
+                //Debug.Log("Checker: spielt ab!");
+                animationScript.Abspielen();
+            }
+            for (int i = 0; i < ToggleObjectState.Length; i++)
+            {
+                Debug.Log("ToggleObjectState[i].SetActive(true) + Object:",ToggleObjectState[i]);
+                ToggleObjectState[i].SetActive(true);
+            }
         }     
     }
     public void OnObjectExited(SelectExitEventArgs args)
@@ -34,8 +48,18 @@ public class SocketChecker : MonoBehaviour
         {
             //Debug.Log("Das korrekte Objekt wurde aus dem Socket entfernt");
             AnimationScript animationScript = grabInteractable.gameObject.GetComponent<AnimationScript>();
-            //Debug.Log("Checker: spielt nicht mehr ab!");
-            animationScript.Abspielen(); // Stoppt die Animation
+            
+            if (animationScript != null){
+                //Debug.Log("Checker: spielt nicht mehr ab!");
+                animationScript.Abspielen(); // Stoppt die Animation
+            }
+
+            for (int i = 0; i < ToggleObjectState.Length; i++)
+            {
+                Debug.Log("ToggleObjectState[i].SetActive(false) + Object:",ToggleObjectState[i]);
+                ToggleObjectState[i].SetActive(false);
+            }
+
         }
     }
 }
