ad7a206b57e597b743269ec9589d4e1640172ae0
Isa <Isabell.Noack@stud.eah-jena.de>
2024-07-19 10:34:43 +0200

    Lightmap fixes

--- Wasser/Assets/Scripts/Interactibles/InteractableScript.cs
+++ Wasser/Assets/Scripts/Interactibles/InteractableScript.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
-using UnityEditor.MPE;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
